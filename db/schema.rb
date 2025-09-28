e51b640d37e6398f1be1e430dcc9d81aa100599a
Avre Barra <avrebarra@gmail.com>
2025-09-28 23:01:48 +0000

    add indexing #llm
    improve the clockout performance with indexing the required fields; used copilot agent to execute and write the code matching my planned item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_28_000100) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_28_000200) do
   create_table "sleep_records", force: :cascade do |t|
     t.integer "user_id", null: false
     t.datetime "clock_in", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_28_000100) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id", "clock_out"], name: "index_sleep_records_on_user_id_and_clock_out"
     t.index ["user_id"], name: "index_sleep_records_on_user_id"
   end
 
